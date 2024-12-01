e2aeaee33598c18e75533ecd048c5849bad8eb79
Jamie <jamie@thanksben.com>
2024-12-01 09:30:21 +0000

    Adds day 1 code

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -40,5 +40,77 @@ In the example list above, the pairs and distances would be as follows:
 To find the total distance between the left list and the right list, add up the distances between all of the pairs you found. In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
 
 Your actual left and right lists contain many location IDs. What is the total distance between your lists?
+
+--- Part Two ---
+
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+
+For these example lists, here is the process of finding the similarity score:
+
+    The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+    The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+    The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+    The fourth number, 1, also does not appear in the right list.
+    The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+    The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
+
 """
 
+def get_difference(sorted_first_list, sorted_second_list):
+    difference = 0
+    for iterator in range(0, len(sorted_first_list)):
+        first_number = int(sorted_first_list[iterator])
+        second_number = int(sorted_second_list[iterator])
+        if first_number > second_number:
+            difference += (first_number - second_number)
+        else:
+            difference += (second_number - first_number)
+    return difference
+
+
+def get_similarity(sorted_first_list, sorted_second_list):
+    score = 0
+    for number in sorted_first_list:
+        times_appeared = sorted_second_list.count(number)
+        score += (number * times_appeared)
+    return score
+
+def day1():
+    first_list = []
+    second_list = []
+    with open('input.txt', 'r') as file:
+        for line in file:
+            first_number, second_number = line.split("  ")
+            first_list.append(int(first_number))
+            second_list.append(int(second_number))
+    sorted_first_list = sorted(first_list)
+    sorted_second_list = sorted(second_list)
+
+    print('Solution to part 1: ')
+    print(get_difference(sorted_first_list, sorted_second_list))
+    print('----------')
+    print('Solution to part 2')
+    print(get_similarity(sorted_first_list, sorted_second_list))
+
+
+if __name__ == '__main__':
+    print(day1())
